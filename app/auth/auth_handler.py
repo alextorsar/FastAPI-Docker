cc11d84ac08d20e8f9263371aabd049c0842f94d
alextorsar <alejandro.torres.sar@gmail.com>
2023-06-13 12:19:39 +0200

    Quitando tiempo expiracion token

--- app/auth/auth_handler.py
+++ app/auth/auth_handler.py
@@ -12,7 +12,6 @@ JWT_ALGORITHM = "HS256"
 def signJWT(user_id: str) -> Dict[str, str]:
     payload = {
         "user_id": user_id,
-        "expires": time.time() + 600
     }
     token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
 
@@ -22,6 +21,6 @@ def signJWT(user_id: str) -> Dict[str, str]:
 def decodeJWT(token: str) -> dict:
     try:
         decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
-        return decoded_token if decoded_token["expires"] >= time.time() else None
+        return decoded_token
     except:
         return {}
